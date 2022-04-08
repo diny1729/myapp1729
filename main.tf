b76864920d27b6434720e76c3946c9ca060a68f2
diny1729 <sdinesh1010@gmail.com>
2022-04-08 00:39:52 -0400

    code added

---
+++ main.tf
@@ -0,0 +1,32 @@
+resource "random_integer" "ri" { 
+  min = 10000
+  max = 33333
+}
+
+resource "azurerm_resource_group" "rg"
+  name = "myrg-${random_integer.ri.result}
+  location = "eastus"
+}
+
+resource "azurerm_app_service_plan" "appserviceplan" {
+  name                = "webapp-asp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku { 
+    tier = "free"
+	size = "f1"
+}
+
+
+resource "azurerm_app_service" "webapp" { 
+  name = "webapp-${random_integer.ri.result}"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+  source_control {
+	repo_url = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+	branch = "master"
+	manual_integration = true
+	use_mercurial = false
+  }
+}
