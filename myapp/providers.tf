e4ae3888246782e0791b2583fc56d4f8d29e918b
diny1729 <sdinesh1010@gmail.com>
2022-04-08 01:53:02 -0400

    test3

--- myapp/providers.tf
+++ myapp/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers { 
     azurerm = {
       source = "hasicorp/azurerm"
-      version = "~> 2.65"
+      version = "2.65"
     }
   }
   required_version = ">= 0.14.9"
